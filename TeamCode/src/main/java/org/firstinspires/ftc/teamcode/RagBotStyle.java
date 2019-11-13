ec9ad8dc47c2394a3c54063c01c2739070e63286
zorbulator <zorbulator@gmail.com>
2019-11-13 09:44:16 -0800

    Undid a previous unnamed commit and made the name clearer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RagBotStyle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RagBotStyle.java
@@ -41,7 +41,7 @@ import java.io.File;
  * 
  */
 
-@TeleOp(name="Ragbot Style (Default)", group="Scotbotics")
+@TeleOp(name="Drive Robot", group="Scotbotics")
 
 public class RagBotStyle extends LinearOpMode {
     /* Declare OpMode members. */
